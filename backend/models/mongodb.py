5ab3d322e0242f834dd8dbb3b156196f3c95e77e
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-06-21 14:48:30 +0200

    Added monogo db connection logic

--- backend/models/mongodb.py
+++ backend/models/mongodb.py
@@ -1,20 +1,58 @@
-from pymongo.mongo_client import MongoClient
+#From this module we need the db connection, loading of the airbnb data, parsing of core data and handling queries
+#We first need
+
+
+
+from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
+from contextlib import contextmanager
 import os
 
 load_dotenv()
-password = os.getenv('MONGODB_PASSWORD')
-username = os.getenv('MONGODB_USERNAME')
-connection_string = os.getenv('MONGODB_CONNECTION_STRNIG')
+mongodb_uri = os.getenv('MONGODB_URI')
 
-uri = f"mongodb+srv://{username}:{password}{connection_string}"
+@contextmanager
+def mongodb_connection(uri):
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    try:
+        yield client
+    finally:
+        client.close()
 
+def airbnb_load():
+    try:
+        client = MongoClient(mongodb_uri, server_api=ServerApi('1'))
+        client.admin.command('ping')
+        print("Connected to MongoDB successfully!")
+        db = client['sample_airbnb']
+        collection = db['listingAndReviews'].find()
+        return client, collection
+    except Exception as e:
+        print(f"An error occurred while connecting to MongoDB: {e}")
+        return None, None
 
-client = MongoClient(connection_string, server_api=ServerApi('1'))
-try:
-    # Send a ping to confirm a successful connection
-    client.admin.command('ping')
-    print("Connected to MongoDB successfully!")
-except Exception as e:
-    print(f"An error occurred while connecting to MongoDB: {e}")
+def upload_document(document, function: str, filter_criteria=None, update_fields=None):
+    client, collection = airbnb_load()
+    
+    if collection is not None:
+        if function == 'Insert':
+            if isinstance(document, list):
+                collection.insert_many(document)
+            else:
+                collection.insert_one(document)
+        
+        elif function == 'Update':
+            if filter_criteria and update_fields:
+                result = collection.update_one(filter_criteria, {'$set': update_fields})
+                if result.modified_count > 0:
+                    print("Document updated successfully!")
+                else:
+                    print("No document matched the criteria. No update performed.")
+            else:
+                print("Filter criteria and update fields are required for update operation.")
+    else:
+        print("Failed to load the collection.")
+    
+    if client:
+        client.close()
