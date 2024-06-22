47578f828cd8cfe96bff8636fd446a3c6d80aeb0
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-06-22 08:04:58 +0200

    mongobd.py update
    Fixed database connection logic. Also added comments for explaining building process

--- backend/models/mongodb.py
+++ backend/models/mongodb.py
@@ -1,27 +1,30 @@
 #From this module we need the db connection, loading of the airbnb data, parsing of core data and handling queries
-#We first need
+#We first need to connect to the database, then retrieve the entries of the airbnb collection.
+#Required Functions:
+# 1. database connection (Done)
+# 2. retrieval of airbnb listing and reviews (Done)
+# 3. if embeddings doesn't exist then we need to create the embeddings
+#    We want to embed the description given by the host for the user to query against
+#    Additional emdebbings could include reviews but for the simple version we just need the 
 
 
 
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
-from contextlib import contextmanager
 import os
 
 load_dotenv()
-mongodb_uri = os.getenv('MONGODB_URI')
 
-@contextmanager
+#Connection to the mongo DB database
 def mongodb_connection(uri):
     client = MongoClient(uri, server_api=ServerApi('1'))
-    try:
-        yield client
-    finally:
-        client.close()
+    return client
 
+#Loading of the air_bnb sample database and reurning the listingss and reviews collection
 def airbnb_load():
     try:
+        mongodb_uri = os.getenv('MONGODB_URI')
         client = MongoClient(mongodb_uri, server_api=ServerApi('1'))
         client.admin.command('ping')
         print("Connected to MongoDB successfully!")
