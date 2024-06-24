bd31542ab2c7ecd03c6ce8c1406a22bd0cdceb1c
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-06-24 07:30:48 +0200

    config settings for tailwind postcss and vite

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}",
     "./index.html",
-    "./src/**/*.{html,js,jsx}",
   ],
   theme: {
     extend: {},
