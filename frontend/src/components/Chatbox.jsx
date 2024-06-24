a34f604828e6ade5624735af10d6e3617efe0e19
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-06-24 07:26:38 +0200

    Added chatbox component

---
+++ frontend/src/components/Chatbox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chatbox = () => {
+  return (
+    <div>
+      <h2>Chatbox</h2>
+      <textarea placeholder="Type your message here..." className="w-full h-24 border"></textarea>
+    </div>
+  );
+};
+
+export default Chatbox;
