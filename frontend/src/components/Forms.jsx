134e957339b84b44e1cdc27d83b05551635b7b33
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-07-19 17:27:29 +0200

    Forms.jsx
    Added basic forms componenet to use for all pages

---
+++ frontend/src/components/Forms.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FormComponent = ({ children, title }) => {
+  return (
+    <div className="w-full h-auto rounded-lg border-0 border-slate-50 shadow-inner shadow-rose-50 mix-blend-normal p-4">
+      {title && <h2 className="text-2xl font-bold mb-6 text-center">{title}</h2>}
+      <form className="grid grid-cold-1 md:grid-cols-2 gap-4">
+        {children}
+      </form>
+    </div>
+  );
+};
+
+export default FormComponent;
