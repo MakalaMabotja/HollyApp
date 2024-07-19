b3b7eff246f2083e993042956f1e0b4f9b8179f2
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-07-19 17:36:33 +0200

    Added PreferencesForm.jsx
    Added preferences box as home page component to get user preferences for holiday types

---
+++ frontend/src/pages/Home/components/PreferencesForm.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import FormComponent from '../../../components/Forms';
+import Chatbox from '../../../components/Chatbox';
+
+const holidayPreferences = [
+  'Go Backpacking',
+  'See The Nightlife',
+  'Be One With Nature',
+  'Spend Time At The Beach',
+  'Partake In Adventure Sports',
+  'Cultural Exploration',
+  'Food & Wine Tours',
+  'Relaxation Getaway',
+  'City Tour',
+  'Boat Cruise',
+];
+
+const PreferencesForm = () => {
+  const [selectedPreferences, setSelectedPreferences] = useState([]);
+
+  const handleCheckboxChange = (preference) => {
+    setSelectedPreferences((prev) => 
+      prev.includes(preference)
+        ? prev.filter((item) => item !== preference)
+        : [...prev, preference]
+    );
+  };
+
+  return (
+    <FormComponent title="What do you feel like doing on your next holiday?" className="justify-center items-center flex flex-col">
+      
+          {holidayPreferences.map((preference, index) => (
+            <div className='flex flex-col justify-center md:flex-1 md:w-full'>
+            <label key={index} className="inline-flex items-center">
+            <input
+                type="checkbox"
+                className="form-checkbox"
+                value={preference}
+                checked={selectedPreferences.includes(preference)}
+                onChange={() => handleCheckboxChange(preference)}
+            />
+            <span className="ml-2">{preference}</span>
+            </label>
+            </div>
+      ))}
+      <div className='md:col-span-2'>
+        <Chatbox/>
+      </div>
+    </FormComponent>
+  );
+};
+
+export default PreferencesForm;
