cf40c6e073d830c3bbd28579864b078e6bacdcf8
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-07-19 17:31:26 +0200

    Updated page panels
    Updated panels component for each page to ensure that they adjust sizing responsively when screen size changes

--- frontend/src/components/CenterPanel.jsx
+++ frontend/src/components/CenterPanel.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const CenterPanel = ({ children }) => {
   return (
-    <div className="w-1/2 bg-white h-screen p-4 overflow-y-auto">
+    <div className="w-full md:w-3/5 bg-white h-auto p-4 overflow-y-auto justify-center items-center">
       {children}
     </div>
   );
--- frontend/src/components/RightPanel.jsx
+++ frontend/src/components/RightPanel.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const RightPanel = ({ children }) => {
   return (
-    <div className="w-1/4 h-screen p-4 overflow-y-auto">
+    <div className="w-full md:w-1/5 h-auto p-4 overflow-y-auto justify-center items-center">
       {children}
     </div>
   );
