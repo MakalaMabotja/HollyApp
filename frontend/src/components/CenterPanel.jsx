6147641b89777cca195f168f093704b7edc338eb
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-07-19 18:39:57 +0200

    Fixed CenterPanel.jsx so the center panel flex-grows

--- frontend/src/components/CenterPanel.jsx
+++ frontend/src/components/CenterPanel.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const CenterPanel = ({ children }) => {
   return (
-    <div className="w-full md:w-3/5 bg-white h-auto p-4 overflow-y-auto justify-center items-center">
+    <div className="w-full md:w-3/5 flex-grow bg-white h-auto p-4 overflow-y-auto justify-center items-center">
       {children}
     </div>
   );
