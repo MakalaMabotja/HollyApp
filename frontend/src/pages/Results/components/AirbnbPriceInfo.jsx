e43755be606aa10d094b11553bdeab0f78d4ab03
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-08-02 12:23:16 +0200

    Updated AirbnbpriceInfo.jsx

--- frontend/src/pages/Results/components/AirbnbPriceInfo.jsx
+++ frontend/src/pages/Results/components/AirbnbPriceInfo.jsx
@@ -2,22 +2,38 @@ import React from 'react';
 import Card from '../../../components/ResultsCard';
 
 const AirbnbPriceInfo = ({ data }) => {
+  const formattedPrice = `$${data.price}`;
   return (
     <Card
       title="Airbnb Listing"
       expandableContent={
         <div>
+          <p>Host Name: {data.hostName}</p>
+          <p>Dates: {data.dates}</p>
           <p>Amenities: {data.amenities}</p>
           <p>Description: {data.description}</p>
         </div>
       }
+      price = {formattedPrice}
     >
-      <p>Host Name: {data.hostName}</p>
-      <p>Listing Name: {data.listingName}</p>
-      <p>Dates: {data.dates}</p>
-      <p>Rooms: {data.rooms}</p>
-      <p>Beds: {data.beds}</p>
-      <p>Bathrooms: {data.bathrooms}</p>
+      <div className="text-center mb-4">
+        <p className="text-lg font-bold">{data.location}</p>
+      </div>
+
+      <div className="flex justify-center items-center mt-auto mb-4">
+        <div className="flex items-center mx-2">
+          <span className="text-gray-600 text-lg" role="img" aria-label="Home">🏠</span>
+          <span className="ml-1">{data.rooms}</span>
+        </div>
+        <div className="flex items-center mx-2">
+          <span className="text-gray-600 text-lg" role="img" aria-label="Bed">🛏️</span>
+          <span className="ml-1">{data.beds}</span>
+        </div>
+        <div className="flex items-center mx-2">
+          <span className="text-gray-600 text-lg" role="img" aria-label="Bathroom">🚿</span>
+          <span className="ml-1">{data.bathrooms}</span>
+        </div>
+      </div>
     </Card>
   );
 };
